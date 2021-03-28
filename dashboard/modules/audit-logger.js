8256a07cb5c7e9a0244eb419c7bb996149797f64
NLB94 <nlbnn3@gmail.com>
2021-03-28 09:15:12 +0200

    Deleted Dashboard

--- dashboard/modules/audit-logger.js
+++
@@ -1,7 +0,0 @@
-module.exports = new class {
-  // async change(id, change) {
-  //   const log = await logs.get(id);
-  //   log.changes.push(change);
-  //   await log.save();
-  // }
-}
--- dashboard/modules/auth-client.js
+++
@@ -1,8 +0,0 @@
-const OAuthClient = require('disco-oauth');
-const config = process.env;
-
-const client = new OAuthClient(config.CLIENT_ID, config.CLIENT_SECRET);
-client.setRedirect(`http://localhost/auth`);
-client.setScopes('identify', 'guilds', 'email');
-
-module.exports = client;
