03542a94c077b469288c7320ba9e16bc626245cc
NLB94 <nlbnn3@gmail.com>
2021-03-31 19:51:57 +0200

    Update react.js

--- Bot/commands/Other/react.js
+++ Bot/commands/Other/react.js
@@ -2,8 +2,8 @@ const { MESSAGES } = require("../../../util/constants");
 const functions = require('../../../util/functions');
 
 module.exports.run = functions.run = async (client, message, args) => {
-    const messages = await message.channel.messages.fetch()
-    const msg = args[0].startsWith('^') ? await messages.resolve(message.channel.lastMessageID) : await messages.resolve(args[0]);
+    await message.channel.messages.fetch()
+    const msg = args[0].startsWith('^') ? await message.channel.messages.resolve(message.channel.lastMessageID) : await message.channel.messages.resolve(args[0]);
     if (msg == undefined || !msg) return message.channel.send('Invalid Message ID !');
 
     const reaction = args[1] ? (isNaN(args[1]) ? message.guild.emojis.cache.find(e => e.name.toLowerCase().startsWith(args[1].toLowerCase())) : message.guild.emojis.resolve(args[1])) : null;
