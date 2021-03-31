94afc6f1d9d4f382d59db7b76900371b169bb111
NLB94 <nlbnn3@gmail.com>
2021-03-31 12:09:30 +0200

    Update gcreate.js

--- Bot/commands/Giveaway/gcreate.js
+++ Bot/commands/Giveaway/gcreate.js
@@ -76,7 +76,7 @@ module.exports.run = functions.run = async (client, message, args) => {
             embed.setFooter(`ID : ${msg.id}`);
             if (msg) msg.edit(embed);
             setInterval(async () => {
-                if (msg.embeds[0].author.name !== embed.author.name && !(ms(giveaway.time) > 10000)) return;
+                if (msg.embeds[0].author.name !== embed.author.name && ms(giveaway.time) <= 10000) return;
                 else {
                     giveaway.time = ms(ms(giveaway.time) - 10000);
                     await embed.setDescription(`${giveaway.winnerCount} winner(s) \nTime remaining : ${ms(ms(giveaway.time))} \nHosted by : ${message.author}`)
