2fe95ed92d6d51883d7ce31c556e5c074902fd28
NLB94 <nlbnn3@gmail.com>
2021-03-31 15:42:51 +0200

    New commands

--- Bot/commands/Info/messages.js
+++ Bot/commands/Info/messages.js
@@ -11,7 +11,7 @@ module.exports.run = functions.run = async (client, message, args) => {
     const settings = await client.getGuild(message.guild);
     const x_mark = client.emojis.resolve('806440609127596032');
     const checkMark = client.emojis.resolve('770980790242377739');
-    const arrowRight = message.guild.me.permissions.has('USE_EXTERNAL_EMOJIS') ? client.emojis.resolve(client.localEmojis.arrowRight) : '\u200b';
+    const arrowRight = message.guild.me.permissions.has('USE_EXTERNAL_EMOJIS') ? client.emojis.resolve('770976808899444776') : '\u200b';
 
     let user = args[0] ? (args[0].startsWith('<@') && args[0].endsWith('>') ? message.mentions.users.first() : (isNaN(args[0]) ? (args[0].includes('#') ? client.users.cache.find(m => m.tag.toLowerCase() == args[0].toLowerCase()) : (message.guild.members.cache.find(m => m.user.username.toLowerCase() == args[0].toLowerCase()) == undefined ? client.users.cache.find(m => m.username.toLowerCase() == args[0].toLowerCase()) : message.guild.members.cache.find(m => m.user.username.toLowerCase() == args[0].toLowerCase()).user)) : client.users.cache.get(args[0]))) : message.author;
 
