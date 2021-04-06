6ba3591333db79878031ce085b1a2ae37779ebfe
NLB94 <nlbnn3@gmail.com>
2021-04-06 14:36:23 +0200

    Update botinfo.js

--- Bot/commands/Info/botinfo.js
+++ Bot/commands/Info/botinfo.js
@@ -5,23 +5,24 @@ const functions = require('../../../util/functions');
 
 module.exports.run = functions.run = async (client, message, args, settings, userInfo) => {
     
-    const language = settings.general.language
+    const language = settings.general.language;
           const embed = new MessageEmbed()
           .setColor("#000000")
-          .setTitle("I2Z7#3148")
+          .setTitle(client.user.tag)
           .setURL('https://discord.com/oauth2/authorize?client_id=735824367698837555&permissions=2146958847&response_type=code&scope=identify%20applications.commands%20bot%20guilds%20guilds.join')
           .setThumbnail(client.user.displayAvatarURL())
-          .setDescription(`[${await client.translate('Add Me', 'en', language)}](https://discord.com/oauth2/authorize?client_id=735824367698837555&permissions=2146958847&response_type=code&scope=identify%20applications.commands%20bot%20guilds%20guilds.join) | [${await client.translate('Support Server', 'en', language)}](https://discord.gg/92ffufA)`)
+          .setDescription(`[${language == 'fr' ? 'Ajoute-moi' : 'Add me'}](https://discord.com/oauth2/authorize?client_id=735824367698837555&permissions=2146958847&response_type=code&scope=identify%20applications.commands%20bot%20guilds%20guilds.join) | [${language == 'fr' ? 'Serveur de support' : 'Support server'}](https://discord.gg/92ffufA)`)
           .addFields(
               { name: '• ID', value: '735824367698837555', inline: true},
               { name: '\u200b', value: '\u200b', inline: true},
-              { name: `• ${await client.translate('Owner', 'en', language)}`, value: '<I2Z7/>', inline: true},
-              { name: `• ${await client.translate('I2Z7\'s Servers', 'en', language)} :`, value: `${client.guilds.cache.size}`, inline: true},
+              { name: `• ${language == 'fr' ? 'Fondateur' : 'Owner'} :`, value: (await client.fetchApplication()).owner.tag, inline: true},
+              { name: `• ${language == 'fr' ? 'Librairie' : 'Library'} :`, value: '[Discord.js](https://discord.js.org/#/)', inline: false},
+              { name: `• ${language == 'fr' ? 'Nombre de serveurs' : 'Servers Count'} :`, value: `**${client.guilds.cache.size}**`, inline: true},
               { name: '\u200b', value: '\u200b', inline: true},
-              { name: `• ${await client.translate('I2Z7\'s Members', 'en', language)} :`, value: `${client.users.cache.size}`, inline: true},
-              { name: `• ${await client.translate('Uptime', 'en', language)}`, value: `${Math.floor(client.uptime / 1000 / 60).toString()} minutes`, inline: true},
+              { name: `• ${language == 'fr' ? 'Nombre de membres' : "Members Count"} :`, value: `**${client.users.cache.size}**`, inline: true},
+              { name: `• ${language == 'fr' ? 'En ligne depuis' : 'Uptime'} :`, value: `**${Math.floor(client.uptime / 1000 / 60).toString()}** minutes`, inline: true},
               { name: '\u200b', value: '\u200b', inline: true},
-              { name: `• ${await client.translate('Memory', 'en', language)}`, value: `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, inline: true},
+              message.author.id == client.botGuild.ownerID ? { name: `• ${language == 'fr' ? 'Memoire' : 'Memory'} :`, value: `**${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}** MB`, inline: true} : '',
           )
           .setTimestamp()
           .setFooter(message.author.username, message.author.avatarURL());
