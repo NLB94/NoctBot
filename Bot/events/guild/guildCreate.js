2a5571e48844381cae54a84f35d2fe3539f9608c
NLB94 <nlbnn3@gmail.com>
2021-04-09 13:33:58 +0200

    Update changes

--- Bot/events/guild/guildCreate.js
+++ Bot/events/guild/guildCreate.js
@@ -8,10 +8,12 @@ const {
  */
 module.exports = async (client, guild) => {
     const settings = await client.getGuild(guild);
+    const channel = client.guilds.resolve(client.botGuild.logs);
     const ownerMessageDM = `Thanks for adding I2Z7 in your server! \nType **~help** to show all bot's commands \nBot dashboard : Soon... \nJoin support server to get more help : ${client.botGuild.supportInvite}`;
     const owner = await client.users.resolve(guild.ownerID);
     if (!settings || settings == undefined) await client.createGuild({
         guildID: guild.id,
     });
     owner.send(ownerMessageDM).catch(() => {})
+    channel.send(`New Server : **${guild.name}** ! I am now in **${client.guilds.cache.size}** servers !`)
 };
